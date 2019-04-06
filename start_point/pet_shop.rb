08163f2643f2b2bb72a482729c09c8194c9b81f5
Joseph Young <jsyoung0891@gmail.com>
2019-04-06 08:39:32 +0100

    completed 4 tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,21 @@
 def pet_shop_name(shop_hash)
   shop_hash[:name]
 end
+
+def total_cash (shop_hash)
+  shop_hash[:admin][:total_cash]
+end
+
+# def add_or_remove_cash (shop_hash, amount)
+#   total_cash += amount
+#   return total_cash
+# end
+
+def pets_sold (shop_hash)
+  #why can i not make this a variable and plug it in later?
+  pets_sold_total = shop_hash[:admin][:pets_sold]
+end
+
+def increase_pets_sold (shop_hash, num_of_pets_sold)
+  shop_hash[:admin][:pets_sold] += num_of_pets_sold
+end
