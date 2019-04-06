2d88ebbedccf1ebfeba6179d01d497515335cbf7
Joseph Young <jsyoung0891@gmail.com>
2019-04-06 16:04:27 +0100

    passed 14 tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,32 +1,32 @@
-def pet_shop_name(shop_hash)
-  shop_hash[:name]
+def pet_shop_name(shop_stock)
+  shop_stock[:name]
 end
 
-def total_cash (shop_hash)
-  shop_hash[:admin][:total_cash]
+def total_cash (shop_stock)
+  shop_stock[:admin][:total_cash]
 end
 ########## REFACTOR WITH VARIABLES! ##############
-def add_or_remove_cash (shop_hash, amount)
-   shop_hash[:admin][:total_cash] += amount
+def add_or_remove_cash (shop_stock, amount)
+   shop_stock[:admin][:total_cash] += amount
 end
 
-def pets_sold (shop_hash)
+def pets_sold (shop_stock)
   #why can i not make this a variable and plug it in later?
-  pets_sold_total = shop_hash[:admin][:pets_sold]
+  pets_sold_total = shop_stock[:admin][:pets_sold]
 end
 
-def increase_pets_sold (shop_hash, num_of_pets_sold)
-  shop_hash[:admin][:pets_sold] += num_of_pets_sold
+def increase_pets_sold (shop_stock, num_of_pets_sold)
+  shop_stock[:admin][:pets_sold] += num_of_pets_sold
 end
 
-def stock_count (shop_hash)
-  shop_hash[:pets].length
+def stock_count (shop_stock)
+  shop_stock[:pets].length
 end
 
 ##### REFACTOR WITH BETTER NAMES! ###########
-def pets_by_breed (shop_hash, breed)
+def pets_by_breed (shop_stock, breed)
   breed_count = []
-  for pet in shop_hash[:pets]
+  for pet in shop_stock[:pets]
     if pet[:breed] == breed
       breed_count << [:breed]
     end
@@ -34,10 +34,27 @@ def pets_by_breed (shop_hash, breed)
   return breed_count
 end
 
-def find_pet_by_name(shop, pet_name)
+def find_pet_by_name(shop_stock, pet_name)
  names = []
-  for pet in shop[:pets]
+  for pet in shop_stock[:pets]
     names.push(pet) if pet[:name] == pet_name
   end
   return names[0]
+end
+
+
+def remove_pet_by_name(shop_stock, pet_to_delete)
+  for pet in shop_stock[:pets]
+     if pet[:name] == pet_to_delete
+       shop_stock[:pets].delete(pet)
+     end
+  end
+end
+
+def add_pet_to_stock(shop_stock, new_pet)
+  shop_stock[:pets].push(new_pet)
+end
+
+def customer_cash(customer_id)
+  customer_id[:cash]
 end
